76f87b7364ca24e1256d967964f795ab60c4200c
Paul Hlusko <paul.hlusko@gmail.com>
2018-02-08 02:47:24 -0500

    working on pendulum

--- core/src/com/p4u1/metronome/MetronomeGame.java
+++ core/src/com/p4u1/metronome/MetronomeGame.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -20,30 +21,45 @@ import java.sql.Time;
 public class MetronomeGame extends ApplicationAdapter {
 	SpriteBatch batch;
     ShapeRenderer shapes;
-	Texture img;
+	Texture pauseTexture;
+	Sprite pause;
+	Texture playTexture;
+	Sprite play;
 	Sound woodblockSound;
-	long bpm = 170;
+	long bpm = 70;
 	long lastTap = 0;
 	static long MIL_IN_MINUTE = 60000;
 
 	long timer = TimeUtils.millis();
 
 	Boolean isTapping = false;
+	Boolean pausing = false;
 	OrthographicCamera camera;
     BitmapFont font;
     long lastFlash = TimeUtils.millis();
+
 	
 	@Override
 	public void create () {
 
 		woodblockSound = Gdx.audio.newSound(Gdx.files.internal("woodblock.wav"));
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		pauseTexture = new Texture("pause.png");
+		playTexture = new Texture("play.png");
+		pause = new Sprite(pauseTexture);
+		play = new Sprite(playTexture);
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, PaulGraphics.GAMEWIDTH, PaulGraphics.GAMEHEIGHT);
         font = new BitmapFont(Gdx.files.internal("droid.fnt"),
                 false);
         shapes = new ShapeRenderer();
+
+		pause.setY(250);
+		pause.setX((PaulGraphics.GAMEWIDTH / 2) - 50);
+
+		play.setY(250);
+		play.setX((PaulGraphics.GAMEWIDTH / 2) - 50);
+		setOffset();
 	}
 
 	@Override
@@ -55,37 +71,56 @@ public class MetronomeGame extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
         font.setColor(Color.WHITE);
         String bpmString = "- " + bpm + " +";
-        //Gdx.app.log("phlusko",font.getScaleX() + "");
-        //Gdx.app.log("phlusko",font.getScaleY() + "");
         float space = font.getSpaceWidth();
-        //font.draw(batch, bpmString, (PaulGraphics.width / 2) - ((bpmString.length() / 2) * 87), (PaulGraphics.height / 2) + 40);
         font.draw(batch, bpmString, (PaulGraphics.GAMEWIDTH / 2) - (bpmString.length() * 20), (PaulGraphics.GAMEHEIGHT / 2) + 25);
         String tap = "Tap";
         font.draw(batch, tap, (PaulGraphics.GAMEWIDTH / 2) - (tap.length() * 20), 117 + 25);
+        if (pausing) {
+			play.draw(batch);
+		} else {
+			pause.draw(batch);
+		}
 		batch.end();
 
 		shapes.begin(ShapeRenderer.ShapeType.Line);
         shapes.setProjectionMatrix(camera.combined);
 		shapes.setColor(Color.WHITE);
-        shapes.line(0, 600, PaulGraphics.GAMEWIDTH, 600);
-        shapes.line(0, 400, PaulGraphics.GAMEWIDTH, 400);
         if (isTapping) {
             shapes.rect(166, 68, 160, 100);
         }
+        long diff = TimeUtils.millis() - timer;
+        float centerX = PaulGraphics.GAMEWIDTH / 2;
+        float centerY = 600;
+        float beatTime = 60000 / bpm;
+
+        double angle = ((2* Math.PI) * ((int)((TimeUtils.millis() - angleOffset) % (int)(2*beatTime)) / (2*beatTime) )) + (Math.PI * .5);
+        shapes.line(centerX, centerY, centerX + (float)Math.cos(angle)*250, centerY + (float)Math.abs(Math.sin(angle))*250);
+		shapes.line(centerX, centerY, centerX, 900);
 		shapes.end();
         shapes.begin(ShapeRenderer.ShapeType.Filled);
-        if (TimeUtils.millis() - lastFlash < 20) {
-            shapes.rect(0, 600, PaulGraphics.GAMEWIDTH, 400);
+        if (TimeUtils.millis() - lastFlash < 30) {
+            //shapes.rect(0, 600, PaulGraphics.GAMEWIDTH, 400);
         }
         shapes.end();
 		logic();
 	}
+	double angleOffset = 0;
+
+	public void setOffset() {
+		long diff = TimeUtils.millis() - timer;
+		float centerX = PaulGraphics.GAMEWIDTH / 2;
+		float centerY = 600;
+		float beatTime = 60000 / bpm;
+
+		angleOffset = TimeUtils.millis() % (int)(2*beatTime);
+	}
 
 	public void logic() {
-		if (TimeUtils.millis() - timer > (MIL_IN_MINUTE / bpm)) {
+		if (TimeUtils.millis() - timer > (MIL_IN_MINUTE / bpm) && !pausing) {
 			woodblockSound.play();
 			timer = TimeUtils.millis();
 			lastFlash = TimeUtils.millis();
+			//setOffset();
 		}
 
         if (Gdx.input.justTouched()) {
@@ -93,8 +128,14 @@ public class MetronomeGame extends ApplicationAdapter {
 		    if (touch.y < 600 && touch.y > 400) {
 		        if (touch.x > (PaulGraphics.GAMEWIDTH / 2)) {
 		            bpm++;
+					timer = TimeUtils.millis();
+					woodblockSound.play();
+					setOffset();
                 } else {
 		            bpm--;
+					timer = TimeUtils.millis();
+					woodblockSound.play();
+					setOffset();
                 }
             }
             if (touch.y < 185) {
@@ -104,23 +145,40 @@ public class MetronomeGame extends ApplicationAdapter {
 		            isTapping = false;
 		            timer = TimeUtils.millis();
 		            woodblockSound.play();
+					setOffset();
                 } else {
                     isTapping = true;
                     lastTap = TimeUtils.millis();
                 }
             }
-            if (TimeUtils.millis() - lastTap > 2000) {
-		        isTapping = false;
-            }
-            Gdx.app.log("phlusko", PaulGraphics.pixelToCoord(new Vector2(Gdx.input.getX(), Gdx.input.getY())).toString());
-            Gdx.app.log("phlusko", new Vector2(Gdx.input.getX(), Gdx.input.getY()).toString());
+
+            if (touch.y < 360 && touch.y > 240) {
+		    	if (pausing) {
+					pausing = false;
+					timer = TimeUtils.millis();
+					woodblockSound.play();
+					lastFlash = TimeUtils.millis();
+					setOffset();
+				} else {
+		    		pausing = true;
+				}
+			}
+
+            Gdx.app.log("phlusko-touch_coordinate", PaulGraphics.pixelToCoord(new Vector2(Gdx.input.getX(), Gdx.input.getY())).toString());
+            Gdx.app.log("phlusko-touch_pixel", new Vector2(Gdx.input.getX(), Gdx.input.getY()).toString());
 
         }
+		if (TimeUtils.millis() - lastTap > 2000) {
+			isTapping = false;
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		shapes.dispose();
+		font.dispose();
+		woodblockSound.dispose();
+		pauseTexture.dispose();
 	}
 }
